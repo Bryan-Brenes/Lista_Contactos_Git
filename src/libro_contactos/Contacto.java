82e61e9a0437c21d07d1cf64d8f0e3b5671afc76
Bryan <bryanbrenes1@gmail.com>
2018-06-10 12:46:14 -0600

    Archivos iniciales
    Se añaden los archivos iniciales al proyecto

---
+++ src/libro_contactos/Contacto.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package libro_contactos;
+
+import java.util.ArrayList;
+import java.util.Map;
+import javafx.scene.image.Image;
+
+/**
+ *
+ * @author bryan
+ */
+public class Contacto {
+    private String nombre;
+    private Image imagen;
+    private ArrayList<String[]> telefonos;
+    private ArrayList<String[]> emails;
+    private Map<Integer,String> sonidos;
+}//<>
