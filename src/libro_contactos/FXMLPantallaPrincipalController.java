0d4ae17822275da8dc2b5f9b15f2f1637d4da90d
Bryan <bryanbrenes1@gmail.com>
2018-06-10 13:27:34 -0600

    Incorporar la funcionalidad de salir

--- src/libro_contactos/FXMLPantallaPrincipalController.java
+++ src/libro_contactos/FXMLPantallaPrincipalController.java
@@ -12,6 +12,9 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -21,9 +24,17 @@ import javafx.scene.control.Label;
 public class FXMLPantallaPrincipalController implements Initializable {
 
     @FXML
-    private Button button;
+    private MenuItem nuevo_btn_menu;
     @FXML
-    private Label label;
+    private MenuItem guardar_btn_menu;
+    @FXML
+    private MenuItem salir_btn_menu;
+    @FXML
+    private MenuItem acercaDe_btn_menu;
+    @FXML
+    private MenuItem manual_btn_menu;
+    @FXML
+    private MenuBar menubar;
 
     /**
      * Initializes the controller class.
@@ -32,9 +43,11 @@ public class FXMLPantallaPrincipalController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
-
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
+    
+    public void salir(){
+        Stage stage = (Stage) menubar.getScene().getWindow();
+        stage.close();
     }
+
     
 }
