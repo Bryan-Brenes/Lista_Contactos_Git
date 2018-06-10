be961630976f364a7eadde030688e41cd664f317
Bryan <bryanbrenes1@gmail.com>
2018-06-10 17:55:39 -0600

    incorporación de botones en pantalla principal

--- src/libro_contactos/Contacto.java
+++ src/libro_contactos/Contacto.java
@@ -8,6 +8,7 @@ package libro_contactos;
 import java.util.ArrayList;
 import java.util.Map;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 /**
  *
@@ -15,8 +16,180 @@ import javafx.scene.image.Image;
  */
 public class Contacto {
     private String nombre;
-    private Image imagen;
+    private ImageView imagen;
     private ArrayList<String[]> telefonos;
     private ArrayList<String[]> emails;
-    private Map<Integer,String> sonidos;
+    private ArrayList<String[]> sonidos;
+    private String direccionFisica;
+    private String lugarTrabajo;
+    private String lugarEstudio;
+    private String notas;
+    private String acercaFamilia;
+    private String profesion;
+    private String fechaCumpleanios;
+    private String fechaAniversario;
+    private String direccionTrabajo;
+    private String entretenimiento;
+    private String deportes;
+    
+    public Contacto(){
+        this.nombre = "";
+        this.imagen = new ImageView();
+        this.telefonos = new ArrayList<>();
+        this.emails = new ArrayList<>();
+        this.sonidos = new ArrayList<>();
+        this.direccionFisica = "";
+        this.lugarTrabajo = "";
+        this.lugarEstudio = "";
+        this.notas = "";
+        this.acercaFamilia = "";
+        this.profesion = "";
+        this.fechaCumpleanios = "";
+        this.fechaAniversario = "";
+        this.direccionTrabajo = "";
+        this.entretenimiento = "";
+        this.deportes = "";
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public ImageView getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(Image imagen) {
+        this.imagen = new ImageView(imagen);
+        this.imagen.setFitHeight(16);
+        this.imagen.setFitWidth(16);
+    }
+
+    public ArrayList<String[]> getTelefonos() {
+        return telefonos;
+    }
+
+    public void setTelefonos(String tipo, String valor) {
+        String[] tel = new String[2];
+        tel[0] = tipo;
+        tel[1] = valor;
+        this.telefonos.add(tel);
+    }
+
+    public ArrayList<String[]> getEmails() {
+        return emails;
+    }
+
+    public void setEmails(String tipo, String valor) {
+        String[] email = new String[2];
+        email[0] = tipo;
+        email[1] = valor;
+        this.emails.add(email);
+    }
+
+    public ArrayList<String[]> getSonidos() {
+        return sonidos;
+    }
+
+    public void setSonidos(String nombre, String url) {
+        String[] sonido = new String[2];
+        sonido[0] = nombre;
+        sonido[1] = url;
+        this.emails.add(sonido);
+    }
+
+    public String getDireccionFisica() {
+        return direccionFisica;
+    }
+
+    public void setDireccionFisica(String direccionFisica) {
+        this.direccionFisica = direccionFisica;
+    }
+
+    public String getLugarTrabajo() {
+        return lugarTrabajo;
+    }
+
+    public void setLugarTrabajo(String lugarTrabajo) {
+        this.lugarTrabajo = lugarTrabajo;
+    }
+
+    public String getLugarEstudio() {
+        return lugarEstudio;
+    }
+
+    public void setLugarEstudio(String lugarEstudio) {
+        this.lugarEstudio = lugarEstudio;
+    }
+
+    public String getNotas() {
+        return notas;
+    }
+
+    public void setNotas(String notas) {
+        this.notas = notas;
+    }
+
+    public String getAcercaFamilia() {
+        return acercaFamilia;
+    }
+
+    public void setAcercaFamilia(String acercaFamilia) {
+        this.acercaFamilia = acercaFamilia;
+    }
+
+    public String getProfesion() {
+        return profesion;
+    }
+
+    public void setProfesion(String profesion) {
+        this.profesion = profesion;
+    }
+
+    public String getFechaCumpleanios() {
+        return fechaCumpleanios;
+    }
+
+    public void setFechaCumpleanios(String fechaCumpleanios) {
+        this.fechaCumpleanios = fechaCumpleanios;
+    }
+
+    public String getFechaAniversario() {
+        return fechaAniversario;
+    }
+
+    public void setFechaAniversario(String fechaAniversario) {
+        this.fechaAniversario = fechaAniversario;
+    }
+
+    public String getDireccionTrabajo() {
+        return direccionTrabajo;
+    }
+
+    public void setDireccionTrabajo(String direccionTrabajo) {
+        this.direccionTrabajo = direccionTrabajo;
+    }
+
+    public String getEntretenimiento() {
+        return entretenimiento;
+    }
+
+    public void setEntretenimiento(String entretenimiento) {
+        this.entretenimiento = entretenimiento;
+    }
+
+    public String getDeportes() {
+        return deportes;
+    }
+
+    public void setDeportes(String deportes) {
+        this.deportes = deportes;
+    }
+    
+    
+    
 }//<>
--- src/libro_contactos/FXMLPantallaPrincipalController.java
+++ src/libro_contactos/FXMLPantallaPrincipalController.java
@@ -5,15 +5,25 @@
  */
 package libro_contactos;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXListView;
+import java.io.FileInputStream;
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.Priority;
 import javafx.stage.Stage;
 
 /**
@@ -35,19 +45,46 @@ public class FXMLPantallaPrincipalController implements Initializable {
     private MenuItem manual_btn_menu;
     @FXML
     private MenuBar menubar;
+    @FXML
+    private JFXListView<Label> listView;
+    @FXML
+    private JFXButton nuevo_btn;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+        
+        listView.setExpanded(true);
+        listView.depthProperty().set(2);
+        
+        for (int i = 0; i < 10; i++) {
+            
+            try{
+                Label lbl = new Label("item " + i);
+                
+                Image imgen = new Image(new FileInputStream("/home/bryan/Imágenes/logos/153744-1.png"));
+                ImageView img = new ImageView(imgen);
+                img.setFitHeight(25);
+                img.setFitWidth(25);
+                lbl.setGraphic(img);
+                //lbl.setGraphic(new ImageView(new Image("libro_contactos/user.png")));
+                listView.getItems().add(lbl);
+                
+                
+            }catch(Exception e){
+                System.out.println("no se encontro la imagen");
+            }
+        }
+    }    //<>
     
+    @FXML
     public void salir(){
         Stage stage = (Stage) menubar.getScene().getWindow();
         stage.close();
     }
+    
 
     
 }
